47f8af95756c19900ad0702596fdb58741eacb28
VasylDykyi <wasyl.dykyi@gmail.com>
2026-01-28 00:42:26 +0200

    Add submodules, fix config

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -3,6 +3,8 @@
 
 PROJECT_ROOT=$(cd "$(dirname "$0")/.." && pwd)
 
+mkdir -p $PROJECT_ROOT/build
+
 source $PROJECT_ROOT/layers/poky/oe-init-build-env $PROJECT_ROOT/build/rpi5
 
 bitbake-layers add-layer $PROJECT_ROOT/layers/meta-openembedded/meta-oe
@@ -12,4 +14,5 @@ bitbake-layers add-layer $PROJECT_ROOT/layers/meta-openembedded/meta-multimedia
 bitbake-layers add-layer $PROJECT_ROOT/layers/meta-wayland
 bitbake-layers add-layer $PROJECT_ROOT/layers/meta-raspberrypi
 bitbake-layers add-layer $PROJECT_ROOT/layers/meta-qt6
-bitbake-layers add-layer $PROJECT_ROOT/layers/meta-charge-station
+bitbake-layers add-layer $PROJECT_ROOT/layers/meta-hmi
+
